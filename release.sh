4f04e604f6d925473d968db91359274d4f5b3c3f
Rafael Benevides <rafabene@gmail.com>
2013-03-18 13:52:35 -0300

    Prepare for plugin deployment

---
+++ release.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+
+# Require BASH 3 or newer
+
+REQUIRED_BASH_VERSION=3.0.0
+
+if [[ $BASH_VERSION < $REQUIRED_BASH_VERSION ]]; then
+  echo "You must use Bash version 3 or newer to run this script"
+  exit
+fi
+
+# Canonicalise the source dir, allow this script to be called anywhere
+DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+# DEFINE
+
+# EAP team email subject
+EMAIL_SUBJECT="\${RELEASEVERSION} of QSTools released"
+# EAP team email To ?
+EMAIL_TO="jdf-dev@lists.jboss.org"
+EMAIL_FROM="\"JDF Publish Script\" <benevides@redhat.com>"
+
+
+# SCRIPT
+
+usage()
+{
+cat << EOF
+usage: $0 options
+
+This script performs a release of the BOMs 
+
+OPTIONS:
+   -s      Snapshot version number to update from
+   -n      New snapshot version number to update to, if undefined, defaults to the version number updated from
+   -r      Release version number
+EOF
+}
+
+notify_email()
+{
+   echo "***** Performing QSTools release notifications"
+   echo "*** Notifying JBoss JDF Dev list team"
+   subject=`eval echo $EMAIL_SUBJECT`
+   echo "Email from: " $EMAIL_FROM
+   echo "Email to: " $EMAIL_TO
+   echo "Subject: " $subject
+   # send email using sendmail
+   printf "Subject: $subject\nSee \$subject :)\n" | /usr/bin/env sendmail -f "$EMAIL_FROM" "$EMAIL_TO"
+}
+
+release()
+{
+   echo "Releasing QSTools version $RELEASEVERSION"
+   $DIR/release-utils.sh -u -o $SNAPSHOTVERSION -n $RELEASEVERSION
+   git commit -a -m "Prepare for $RELEASEVERSION release"
+   git tag -a $RELEASEVERSION -m "Tag $RELEASEVERSION"
+   git branch $RELEASEVERSION tags/$RELEASEVERSION
+   $DIR/release-utils.sh -r
+   $DIR/release-utils.sh -u -o $RELEASEVERSION -n $NEWSNAPSHOTVERSION
+   git commit -a -m "Prepare for development of $NEWSNAPSHOTVERSION"
+   git push upstrem HEAD --tags
+   echo "***** JBoss QSTools released"
+   read -p "Do you want to send release notifcations to $EAP_EMAIL_TO[y/N]? " yn
+   case $yn in
+       [Yy]* ) notify_email;;
+       * ) exit;
+   esac
+   echo "Don't forget to push the tag and the branch"
+   echo "   git push --tags upstream refs/heads/$RELEASEVERSION"
+}
+
+SNAPSHOTVERSION="UNDEFINED"
+RELEASEVERSION="UNDEFINED"
+NEWSNAPSHOTVERSION="UNDEFINED"
+
+while getopts “n:r:s:” OPTION
+
+do
+     case $OPTION in
+         h)
+             usage
+             exit
+             ;;
+         s)
+             SNAPSHOTVERSION=$OPTARG
+             ;;
+         r)
+             RELEASEVERSION=$OPTARG
+             ;;
+         n)
+             NEWSNAPSHOTVERSION=$OPTARG
+             ;;
+         [?])
+             usage
+             exit
+             ;;
+     esac
+done
+
+if [ "$NEWSNAPSHOTVERSION" == "UNDEFINED" ]
+then
+   NEWSNAPSHOTVERSION=$SNAPSHOTVERSION
+fi
+
+if [ "$SNAPSHOTVERSION" == "UNDEFINED" -o  "$RELEASEVERSION" == "UNDEFINED" ]
+then
+   echo "\nMust specify -r and -s\n"
+   usage
+else  
+   release
+fi
+
+
