9af9c38d4ac0da8f30ccee66396c9d6614ac3645
Paul Robinson <paul.robinson@redhat.com>
2013-11-26 18:40:34 +0000

    Fixed LicenseFixer bug that occurs when 'properties' element missing

--- src/main/java/org/jboss/maven/plugins/qstools/fixers/LicenseFixer.java
+++ src/main/java/org/jboss/maven/plugins/qstools/fixers/LicenseFixer.java
@@ -1,3 +1,19 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.maven.plugins.qstools.fixers;
 
 import javax.xml.xpath.XPathConstants;
@@ -28,10 +44,9 @@ public class LicenseFixer extends AbstractBaseFixerAdapter {
             // Create <licenses/> if it doesn't exists
             if (licensesElement == null) {
                 licensesElement = doc.createElement("licenses");
-
-                Element properties = (Element) getxPath().evaluate("/project/properties", doc, XPathConstants.NODE);
-                properties.getParentNode().insertBefore(licensesElement, properties);
-                properties.getParentNode().insertBefore(doc.createTextNode("\n\n    "), properties); // LF + LF + 4 spaces
+                Element projectElement = (Element) getxPath().evaluate("/project", doc, XPathConstants.NODE);
+                projectElement.appendChild(doc.createTextNode("\n\n    "));
+                projectElement.appendChild(licensesElement);
             }
             Element license = doc.createElement("license");
 
